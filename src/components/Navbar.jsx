ed61b3ebdca2099856ed0d9d0f0da221d0fca725
Supreetha Kharvi <suprithakharvi99@gmail.com>
2026-02-08 12:01:48 +0530

    2nd page

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,6 +1,6 @@
-import {Link} from 'react-router-dom'
+import {Link, useNavigate} from 'react-router-dom'
 import { assets } from '../assets/assets'
-import { Menu, SearchIcon, XIcon } from "lucide-react";
+import { Menu, SearchIcon, TicketPlus, XIcon } from "lucide-react";
 import { useState } from 'react';
 import { useClerk, UserButton, useUser } from '@clerk/clerk-react';
 
@@ -12,6 +12,8 @@ const Navbar = () => {
   const [isOpen, setIsOpen] = useState(false)
   const {user} = useUser()
   const {openSignIn} = useClerk()
+
+  const navigate = useNavigate()
 
   return (
     <div className='fixed top-0 left-0 z-50 w-full flex items-center justify-between px-6 md:px-16 lg:px-36 py-5'>
@@ -39,7 +41,11 @@ const Navbar = () => {
             onClick={openSignIn}
              className='px-4 py-1 sm:px-7 sm:py-2 bg-red-600 hover:bg-red-700 transition rounded-full font-medium cursor-pointer'>Login</button>
           ) : (
-            <UserButton />
+            <UserButton>
+              <UserButton.MenuItems>
+                <UserButton.Action label='My Bookings' labelIcon={<TicketPlus width={15}/>} onClick={()=> navigate('/my-bookings')}/>
+              </UserButton.MenuItems>
+            </UserButton>
           )
         }
         

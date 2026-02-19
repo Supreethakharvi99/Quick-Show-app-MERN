840479034ab1ab399c7e4c1595a92af2f82f9582
Supreetha Kharvi <suprithakharvi99@gmail.com>
2026-02-19 14:46:53 +0530

    added

--- src/lib/timeFormat.js
+++
@@ -1,7 +0,0 @@
-const timeFormat = (minutes) => {
-    const hours = Math.floor(minutes / 60);
-    const minutesReminder = minutes % 6;
-    return `${hours}h ${minutesReminder}m`
-}
-
-export default timeFormat;
